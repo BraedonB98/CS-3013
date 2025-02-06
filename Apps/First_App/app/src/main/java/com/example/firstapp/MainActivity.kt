dba5f6c62dae482b47c259a1b57181c15a9b4614
Braedon Bellamy <42876847+BraedonB98@users.noreply.github.com>
2025-02-06 01:40:28 +0000

    adding logging

--- Apps/First_App/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ Apps/First_App/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -11,9 +11,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-		Toast.makeText(this, "Going to the internet ===" , Toast.LENGTH_LONG).show()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, log.eHere I Am ===" , Toast.LENGTH_LONG).show()
+        // log.<> (<tag>, <Text>,)
+        log.e("Two Out", "=== Up n At Em Error ===")
+        log.wtf("Two Out", "=== Up n At Em Error ===")
+        log.println(7,"Two_Out", "<<<<<<<<<<HERE I AM >>>>>>>>>")//ASSERTS level
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
