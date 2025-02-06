e7a448ebc60a5fe85352579a3d2efb36d52c9163
Braedon Bellamy <42876847+BraedonB98@users.noreply.github.com>
2025-02-05 18:15:05 -0700

    Update MainActivity.kt
    adding comments from lecture

--- Apps/First_App/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ Apps/First_App/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.firstapp
 
+//import android.widget.Toast
+//import android.___.webView//!finish this
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+						//Toast.makeToast(this, "")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
